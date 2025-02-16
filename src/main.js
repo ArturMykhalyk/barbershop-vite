5115e6f43f6cc41c26d3e352b7f8f00eabf70000
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-02-16 16:21:53 +0200

    hero bg

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import '//code.jquery.com/jquery-1.11.0.min.js'
+   import      '//code.jquery.com/jquery-migrate-1.2.1.min.js' 
+   import 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js'    
+   import '/js/slider.js'
+  
